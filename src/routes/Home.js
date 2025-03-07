8dcd52aeb49724835ee97997f34ae3316a644751
manh0912hehe <huumanhtqt2002@gmail.com>
2025-03-07 19:07:43 +0700

    latesst

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     const { isLoggedIn } = this.props;
 
-    let linkToRedirect = isLoggedIn ? "/home" : "";
+    let linkToRedirect = isLoggedIn ? "/system" : "home";
 
     return <Navigate to={linkToRedirect} replace />;
   }
